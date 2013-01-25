c3e02b2f812eb03652666b987534f063ee21b341
syamn <admin@sakura-server.net>
2013-01-26 00:18:42 +0900

    Notify to moderation team when detected trap tower

--- src/main/java/net/syamn/sakuracmd/listener/EntityListener.java
+++ src/main/java/net/syamn/sakuracmd/listener/EntityListener.java
@@ -7,6 +7,7 @@ package net.syamn.sakuracmd.listener;
 import java.util.List;
 
 import net.syamn.sakuracmd.SakuraCmd;
+import net.syamn.sakuracmd.utils.plugin.SakuraCmdUtil;
 import net.syamn.utils.LogUtil;
 import net.syamn.utils.StrUtil;
 import net.syamn.utils.Util;
@@ -159,7 +160,7 @@ public class EntityListener implements Listener{
         }
         
         final Entity ent = event.getEntity();
-        final List<Entity> ents = ent.getNearbyEntities(0.75D, 1.5D, 0.75D);
+        final List<Entity> ents = ent.getNearbyEntities(0.75D, 1.75D, 0.75D);
         
         int i = 0;
         for (final Entity e : ents){
@@ -169,7 +170,10 @@ public class EntityListener implements Listener{
         if (i >= 3){
             event.setDroppedExp(0);
             event.getDrops().clear();
-            LogUtil.warning("ExpTrap detected! " + StrUtil.getLocationString(ent.getLocation()));
+            
+            String locStr = StrUtil.getLocationString(ent.getLocation().getBlock());
+            SakuraCmdUtil.sendlog("&6経験値トラップを検出: " + locStr);
+            LogUtil.warning("ExpTrap detected at " + locStr);
         }
     }
     
