c84b345ca14adf83fbf4d5e3cdb46c34716ac753
syamn <admin@sakura-server.net>
2013-01-31 23:33:37 +0900

    Fix NBT edit commands, Add way to edit skull owner

--- src/main/java/net/syamn/sakuracmd/commands/other/AdminCommand.java
+++ src/main/java/net/syamn/sakuracmd/commands/other/AdminCommand.java
@@ -8,8 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import net.syamn.sakuracmd.commands.BaseCommand;
 import net.syamn.sakuracmd.permission.Perms;
@@ -25,7 +27,7 @@ public class AdminCommand extends BaseCommand {
     public AdminCommand() {
         bePlayer = false;
         name = "admin";
-        perm = Perms.TRUST;
+        perm = Perms.ADMIN;
         argLength = 1;
         usage = "<- admin commands";
     }
@@ -42,9 +44,9 @@ public class AdminCommand extends BaseCommand {
         }
         
         // item
-        if (sub.equals("item") && isPlayer && args.size() > 2){
+        if (sub.equals("item") && isPlayer && args.size() > 0){
             final String action = args.remove(0).trim().toLowerCase(Locale.ENGLISH);
-            final String str = Util.coloring(StrUtil.join(args, " "));
+            final String str = (args.size() > 0) ? Util.coloring(StrUtil.join(args, " ")) : null;
             
             ItemStack is = player.getItemInHand();
             if (is == null){
@@ -53,9 +55,18 @@ public class AdminCommand extends BaseCommand {
             ItemMeta meta = is.getItemMeta();
             
             if (action.equals("name")){
+                if (str == null){
+                    throw new CommandException("&cメッセージが入力されていません！");
+                }
                 meta.setDisplayName(str);
             }else if (action.equals("add")){
+                if (str == null){
+                    throw new CommandException("&cメッセージが入力されていません！");
+                }
                 List<String> lores = meta.getLore();
+                if (lores == null){
+                    lores = new ArrayList<String>();
+                }
                 lores.add(str);
                 meta.setLore(lores);
             }else if (action.equals("clear")){
@@ -66,6 +77,27 @@ public class AdminCommand extends BaseCommand {
             is.setItemMeta(meta);
             player.setItemInHand(is);
             Util.message(sender, "&aNBTを編集しました！");
+            return;
+        }
+        
+        // head
+        if (sub.equals("head") && isPlayer && args.size() > 0){
+            final String name = args.get(0).trim();
+            if (!StrUtil.isValidName(name)){
+                throw new CommandException("&cプレイヤー名が不正です！");
+            }
+            
+            ItemStack is = player.getItemInHand();
+            if(is == null || is.getType() != Material.SKULL_ITEM){
+                throw new CommandException("&cプレイヤーヘッドを持っていません！");
+            }
+            
+            is.setDurability((short) 3);
+            SkullMeta meta = (SkullMeta) is.getItemMeta();
+            meta.setOwner(name);
+            is.setItemMeta(meta);
+            player.setItemInHand(is);
+            Util.message(sender, "&aプレイヤーヘッドを " + name + " に変更しました！");
             return;
         }
         
